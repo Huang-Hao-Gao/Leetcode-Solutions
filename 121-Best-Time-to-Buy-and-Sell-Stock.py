b7fd322b0844e26e27ce84e993b02e84b37aa858
Huang Hao Gao <98225466+Huang-Hao-Gao@users.noreply.github.com>
2025-01-15 16:36:45 +0000

    get rid of unnecessary comments in 121

--- 121-Best-Time-to-Buy-and-Sell-Stock.py
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,13 +1,5 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #buy, sell variables
-        #assign buy to 1st element
-        # if next element is greater than cur buy date, make that element the sell date
-        # if next element is less than cur buy date, change cur buy date to that element
-        # now that both buy and sell date have been initialised:
-        # if the following element x: buy < x < sell, ignore it
-        # if x > sell, x = sell
-        # if x < buy, push it onto a stack where it's the current best buy, but then pop it if there's no better sell option available so that the previous best buy and sell date is preserved
         buy = prices[0]
         sell = None
         curBest = 0
